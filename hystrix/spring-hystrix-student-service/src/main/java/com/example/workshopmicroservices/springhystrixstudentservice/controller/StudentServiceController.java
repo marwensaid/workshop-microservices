f48858669ecac053711d5f57abf29f59a256ee53
Marwen <marwensaidi1@gmail.com>
2020-09-29 14:10:29 +0200

    Update StudentServiceController.java

--- hystrix/spring-hystrix-student-service/src/main/java/com/example/workshopmicroservices/springhystrixstudentservice/controller/StudentServiceController.java
+++ hystrix/spring-hystrix-student-service/src/main/java/com/example/workshopmicroservices/springhystrixstudentservice/controller/StudentServiceController.java
@@ -17,36 +17,14 @@ public class StudentServiceController {
 	private static Map<String, List<Student>> schooDB = new HashMap<String, List<Student>>();
 
 	static {
-		schooDB = new HashMap<String, List<Student>>();
-
-		List<Student> lst = new ArrayList<Student>();
-		Student std = new Student("Sajal", "Class IV");
-		lst.add(std);
-		std = new Student("Lokesh", "Class V");
-		lst.add(std);
-
-		schooDB.put("abcschool", lst);
-
-		lst = new ArrayList<Student>();
-		std = new Student("Kajal", "Class III");
-		lst.add(std);
-		std = new Student("Sukesh", "Class VI");
-		lst.add(std);
-
-		schooDB.put("xyzschool", lst);
+// create static bd
 
 	}
 
-	@RequestMapping(value = "/getStudentDetailsForSchool/{schoolname}", method = RequestMethod.GET)
+	// rest annotation 
 	public List<Student> getStudents(@PathVariable String schoolname) {
 		System.out.println("Getting Student details for " + schoolname);
+		
+		// method core to get student and manage if null
 
-		List<Student> studentList = schooDB.get(schoolname);
-		if (studentList == null) {
-			studentList = new ArrayList<Student>();
-			Student std = new Student("Not Found", "N/A");
-			studentList.add(std);
-		}
-		return studentList;
-	}
 }
