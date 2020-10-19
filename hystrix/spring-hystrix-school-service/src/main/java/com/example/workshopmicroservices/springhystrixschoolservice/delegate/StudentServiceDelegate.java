92b5e65aa27e5f8072298f978dd62e8cd20f5a5d
marwen.saidi <marwen.saidi@orange.com>
2020-10-19 07:02:15 +0200

    test

--- hystrix/spring-hystrix-school-service/src/main/java/com/example/workshopmicroservices/springhystrixschoolservice/delegate/StudentServiceDelegate.java
+++ hystrix/spring-hystrix-school-service/src/main/java/com/example/workshopmicroservices/springhystrixschoolservice/delegate/StudentServiceDelegate.java
@@ -16,27 +16,13 @@ public class StudentServiceDelegate {
 	RestTemplate restTemplate;
 	
 	// implement a callStudentService method and enabled it by hystrix. we wanna return student detail with a normal flow
+	// this a http exchange
 
 	public String callStudentServiceAndGetData(String schoolname){
 
-		System.out.println("Getting School details for " + schoolname);
-		String response = restTemplate
-				.exchange("http://localhost:8098/getStudentDetailsForSchool/{schoolname}"
-						, HttpMethod.GET
-						, null
-						, new ParameterizedTypeReference<String>() {
-						}, schoolname).getBody();
-
-		System.out.println("Response Received as " + response + " -  " + new Date());
-
-		return "NORMAL FLOW !!! - School Name -  " + schoolname + " :::  Student Details " + response + " -  " + new Date();
+		return "";
 	}
 	// implement a fallback method
-	@SuppressWarnings("unused")
-	private String callStudentServiceAndGetData_Fallback(String schoolname) {
-		System.out.println("Student Service is down!!! fallback route enabled...");
-		return "CIRCUIT BREAKER ENABLED!!!No Response From Student Service at this moment. Service will be back shortly - " + new Date();
-	}
 
 	@Bean
 	public RestTemplate restTemplate() {
